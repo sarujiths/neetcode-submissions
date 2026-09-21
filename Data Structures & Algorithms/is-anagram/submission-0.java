8a9e1fbabbcd8767c064c787f92fb671864e8645
sarujiths <68889008+sarujiths@users.noreply.github.com>
2026-09-21 20:28:32 +0530

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character, Integer> map1 = getOccurrences(s);
+        HashMap<Character, Integer> map2 = getOccurrences(t);
+        return map1.equals(map2);
+    }
+
+    private HashMap<Character, Integer> getOccurrences(String input) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char ch : input.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+        return map;
+    }
+}
